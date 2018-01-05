d05baf42bbbca988bcbcfcf12a8e7ebff123d551
Aren <lenk.legend@outlook.com>
2018-01-05 22:32:33 +0800

    change social conf

--- config/social.php
+++ config/social.php
@@ -1,27 +1,20 @@
 <?php
 
 return [
-/**
- * Created by PhpStorm.
- * User: lufficc
- * Date: 2016/8/21
- * Time: 22:47
- */
-    'facebook' => [
-        'url'  => 'https://www.facebook.com/lufficc',
-        'icon'   => 'facebook',
-    ],
-    'twitter'  => [
-        'url'  => 'https://twitter.com/lufficc',
-        'icon'   => 'twitter',
-    ],
+//    'facebook' => [
+//        'url'  => '',
+//        'icon'   => 'facebook',
+//    ],
+//    'twitter'  => [
+//        'url'  => '',
+//        'icon'   => 'twitter',
+//    ],
     'weibo'    => [
-        'url'  => 'http://weibo.com/u/2363498941',
+        'url'  => 'https://weibo.com/linkus',
         'icon'   => 'weibo',
     ],
     'github'  => [
-        'url' =>'https://github.com/lufficc',
+        'url' =>'https://github.com/lenkaren',
         'icon'   => 'github',
     ],
-
 ];
